c8eccfa3091db27e309868b2d69fbb8bc5d78cad
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-11 02:10:26 +0530

    making app.py

--- app.py
+++ app.py
@@ -7,7 +7,20 @@ app.config['DEBUG']=True
 @app.route('/', methods=['GET','POST'])
 def index():
     url='http://api.openweathermap.org/data/2.5/weather?q={}&appid=532c9007b961c01a0ab55a56f575b8fe'
-    return render_templates('weather.html')
+    city='Las vegas'
+    r=requests.get(url.format(city)).json()
+
+
+    weather={
+       'city': city,
+       'temperture':r['main']['temp'],
+       'description':r['weather'][0]['description'],
+       'icon':r['weather'][0]['icon'],
+
+    }
+    print(weather)
+    return render_templates('weather.html',weather=weather )
+
 
 
 if __name__=="__main__":
