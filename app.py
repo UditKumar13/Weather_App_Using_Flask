448cda025a8c1db1951f9d3bfb86fbe317cc1bc3
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-10 15:33:24 +0530

    url api key

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-from flask import Flask
-from flask_squalchemy import SQLAlchemy
+import requests
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+app.config['DEBUG']=True
+@app.route('/', methods=['GET','POST'])
+def index():
+    url='http://api.openweathermap.org/data/2.5/weather?q={}&appid=532c9007b961c01a0ab55a56f575b8fe'
+    return render_templates('weather.html')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
