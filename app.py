586d0fa1c9550b11ef1d4b30b736514bb2415e22
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-13 19:08:26 +0530

    making database for multiple cities

--- app.py
+++ app.py
@@ -2,21 +2,22 @@ import requests
 from flask import Flask,render_template,json,config
 from flask_sqlalchemy import SQLAlchemy
 
+
+
 app=Flask(__name__)
 app.config['DEBUG']=True
-app.config['SQLAlCHEMY_DATABASE_URI']='sqlite:///showing.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+
 
 
 '''let us make a data base for multiple cities, we will use SQLAlchemy function of flask  '''
 db= SQLAlchemy(app)
 
 class City(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(50),unique=True,nullable=False)
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(50),unique=True,nullable=False)
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+
 
 
 @app.route('/weather_info', methods=['GET','POST'])
