631dbdd584ad8750a3bebd1d46727b73cfb433eb
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-14 01:31:41 +0530

    making placeholder work

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask,render_template,json,config
+from flask import Flask,render_template,json,config,request
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -18,22 +18,27 @@ class City(db.Model):
     name = db.Column(db.String(50),unique=True,nullable=False)
 
 
-
-
 @app.route('/weather_info', methods=['GET','POST'])
 def weather_info():
+
+    if request.method == 'POST':
+        new_city=request.form.get('city')
+        if new_city:
+            new_city_obj=City(name=new_city)
+            db.session.add(new_city_obj)
+            db.session.commit()
+
     cities=City.query.all()
     url='http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=532c9007b961c01a0ab55a56f575b8fe'
     weather_data=[]
     for city in cities:
-        r=requests.get(url.format(city.name)).json()
+        r = requests.get(url.format(city.name)).json()
         weather={
-           'city': city,
-           'temperature':r["main"]['temp'],
-           'humidity':r['main']['humidity'],
-           'description':r['weather'][0]['description'],
-           'icon':r['weather'][0]['icon'],
-
+           'city': city.name,
+           'temperature': r['main']['temp'],
+           'humidity': r['main']['humidity'],
+           'description': r['weather'][0]['description'],
+           'icon': r['weather'][0]['icon'],
         }
         weather_data.append(weather)
 
